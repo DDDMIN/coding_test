7a86c1dd7c553053d238955173551ee7dfa3d269
DDDMIN <78799642+DDDMIN@users.noreply.github.com>
2021-02-10 22:02:38 +0900

    Create 6-1.py

---
+++ 6-1.py
@@ -0,0 +1,104 @@
+#6-1 선택 정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
+
+print(array)
+
+#6-2 삽입 정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1): #i에서 1까지 -1씩 감소
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:
+            break
+print(array)
+
+#6-4 퀵 정렬
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array, start, end):
+    if start >= end: # 원소가 1개인 경우 종료
+        return
+    pivot = start #피벗은 첫 번째 원소
+    left = start + 1
+    right = end
+    while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while right > start and array[right] >= array[pivot]:
+            right -= 1
+        if left > right: #엇갈렸다면 작은 데이터와 피벗을 교체
+            array[right], array[pivot] = array[pivot], array[right]
+        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            array[left], array[right] = array[right], array[left]
+    #분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
+
+#6-5 파이썬 퀵 정렬 소스코드
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot] #[1]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
+
+#6-6 계수 정렬 소스코드
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end= ' ')
+
+#6-7 py sorted 소스코드
+
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+result = sorted(array)
+print(result)
+
+#6.8 py sort 소스코드
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+array.sort()
+print(array)
+
+#6.9 key를 활용한 소스코드
+array = [('바나나', 2), ('사과', 5), ('당근', 3)]
+
+def setting(data):
+    return(data[1])
+
+result = sorted(array, key = setting)
+print(result)
+
+
+#[1] [출력표현식 for 요소 in 입력Sequence [if 조건식]]
+# list = [i*i for i in oldlist if type(i)==int]
+# print(newlist)
+# # 출력: [1, 4, 9]
