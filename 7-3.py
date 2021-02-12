d307f352ee398d189636cdd462fbc9c34b90c55f
DDDMIN <78799642+DDDMIN@users.noreply.github.com>
2021-02-12 21:38:32 +0900

    Create 7-3.py

---
+++ 7-3.py
@@ -0,0 +1,23 @@
+#7-3 이진탐색 코드 반복문
+
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        # mid = target
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+n, target = map(int, input().split())
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n -1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
